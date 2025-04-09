91d2f8f80fc4e10fd279b7b3262d26a310b36443
ProfMatiasJr <antoniomatiassjr@gmail.com>
2025-04-09 16:23:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById("meuFormulario").addEventListener("submit", async (e) =>
   statusMensagem.textContent = "Enviando...";
 
   const formData = new FormData(e.target);
-  const response = await fetch("https://formspree.io/f/antoniomatiassjr@gmail.com", {
+  const response = await fetch("https://formspree.io/f/{antoniomatiassjr@gmail.com}", {
     method: "POST",
     headers: {
       "Accept": "application/json",
